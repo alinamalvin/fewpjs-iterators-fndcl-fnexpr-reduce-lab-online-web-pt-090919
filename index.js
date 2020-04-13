25cffb18130783998e2dfb072f7e475daec55b3a
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-13 00:54:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-let totalBatteries = [1, 2, 3].reduce(function(total, element){ return element * 2 + total}, 10)
+let totalBatteries = batteryBatches.reduce(function(total, element){ return element * 2 + total}, 10)
 // => 22
